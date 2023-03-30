9f8af2afbe1b3d110603af44ca31d3789a0f6e09
aprxty3 <ajiprxty3@gmail.com>
2023-03-30 13:55:35 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/learn_some_package_widget/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.learn_some_package_widget
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:learn_some_package_widget/package_widget/textfield_validator.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(title: 'Material App', home: ListOfWidget());
+  }
+}
+
+class ListOfWidget extends StatelessWidget {
+  const ListOfWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('List Of Widget'),
+      ),
+      body: GridView.count(
+        crossAxisCount: 2,
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
+        padding: const EdgeInsets.all(20),
+        children: [
+          InkWell(
+            onTap: () => Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => const TextFieldValidator(),
+              ),
+            ),
+            borderRadius: BorderRadius.circular(20),
+            child: Material(
+              color: Colors.lightBlueAccent,
+              borderRadius: BorderRadius.circular(20),
+              child: const Center(
+                child: Text('TextField Validator'),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
